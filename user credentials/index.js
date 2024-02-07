3d802df72e4834e4afb060bbe97f7b378c5eec59
leo-nard3 <149709820+leo-nard3@users.noreply.github.com>
2024-02-07 20:12:28 +0300

    Add files via upload

---
+++ user credentials/index.js
@@ -0,0 +1,13 @@
+function userCredentials(email, password){
+    email = "niwagabaleonard@gmail.com";
+    password = 2220562;
+    if (email=="niwagabaleonard@gmail.com" && password == 2220562){
+        console.log('login successful');
+    } else{
+        console.log('incorrect email or password');
+    }
+}
+userCredentials( "niwagabaleonard@gmail.com", 2220562);
+    
+
+
